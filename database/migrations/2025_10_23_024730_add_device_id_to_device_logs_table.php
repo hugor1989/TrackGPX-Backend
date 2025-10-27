da45053aab21e17b24ebc95313dcaa4223bffa1f
Hugo Rodriguez <86859797+hugor1989@users.noreply.github.com>
2025-10-26 23:29:49 -0600

    fix migration devices_logs

--- database/migrations/2025_10_23_024730_add_device_id_to_device_logs_table.php
+++ database/migrations/2025_10_23_024730_add_device_id_to_device_logs_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('device_logs', function (Blueprint $table) {
-            $table->foreignId('device_id')->constrained()->onDelete('cascade');
             $table->string('action');
             $table->text('description')->nullable();
             $table->string('ip_address')->nullable();
@@ -26,7 +25,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('device_logs', function (Blueprint $table) {
-            $table->dropForeign(['device_id']);
             $table->dropColumn(['device_id', 'action', 'description', 'ip_address', 'user_agent']);
         });
     }
